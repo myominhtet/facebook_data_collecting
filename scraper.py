ed67a1adc01cc61048d5a76c4473567025d0e659
myominhtet <myominhtet323@gmail.com>
2024-09-23 11:44:02 +0630

    again

--- scraper.py
+++ scraper.py
@@ -45,12 +45,12 @@ if __name__ == "__main__":
 
 chrome_options = webdriver.ChromeOptions()
 chrome_service = Service("/usr/local/bin/chromedriver")
-chrome_options.add_argument("--incognito")
+# chrome_options.add_argument("--incognito")
 chrome_options.add_argument("--disable-notifications")
 chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-gpu")
+# chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("window-size=1920,1080")
+# chrome_options.add_argument("window-size=1920,1080")
 chrome_options.binary_location = "/usr/bin/google-chrome-stable"
 # Initialize the driver with timeout values
 driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
