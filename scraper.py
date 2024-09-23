b0bea9cc12a3b6ab04d69ca63a6fd649292a4702
myominhtet <myominhtet323@gmail.com>
2024-09-23 11:37:18 +0630

    last update

--- scraper.py
+++ scraper.py
@@ -7,6 +7,7 @@ from bs4 import BeautifulSoup
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
 import os
 import csv
 from argparse import ArgumentParser
@@ -44,12 +45,13 @@ if __name__ == "__main__":
 
 chrome_options = webdriver.ChromeOptions()
 chrome_service = Service("/usr/local/bin/chromedriver")
-#chrome_options.binary_location = "/usr/bin/google-chrome-stable"
 chrome_options.add_argument("--incognito")
 chrome_options.add_argument("--disable-notifications")
-chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("window-size=1920,1080")
+#chrome_options.binary_location = "/usr/bin/google-chrome-stable"
 # Initialize the driver with timeout values
 driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
 driver.set_page_load_timeout(30)  # Set page load timeout in seconds
@@ -70,16 +72,7 @@ driver.find_element(By.XPATH, '//*[@id="email"]').send_keys(args.username)
 driver.find_element(By.XPATH, '//*[@id="pass"]').send_keys(args.password)
 driver.find_element(By.XPATH, '//*[@id="loginbutton"]').click()
 time.sleep(3)
-# Handle Notifications
-# try:
-#     allow_button = WebDriverWait(driver, 10).until(
-#         EC.element_to_be_clickable((By.XPATH, '//*[text()="Allow"]'))
-#     )
-#     allow_button.click()
-# except Exception as e:
-#     print("No notification block appeared or an error occurred:", e)
 
-# Navigate to the specified page or profile link
 driver.get(args.link)
 time.sleep(3)
 
@@ -90,11 +83,12 @@ total_text_saved = 0
 def extract_new_posts():
     global data
     global total_text_saved
-    new_data = []  # To store new posts found during this execution
-
-    # Scroll to load more posts
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(3)  # Wait for posts to load
+    new_data = []  
+    
+    # Scroll multiple times to load more posts
+    for _ in range(5):  # Adjust the number of scrolls based on how many posts you want to load
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(3)
 
     # Find all posts on the page
     posts = driver.find_elements(By.CSS_SELECTOR, ".xdj266r.x11i5rnm.xat24cr.x1mh8g0r.x1vvkbs.x126k92a") # Update this selector as needed
